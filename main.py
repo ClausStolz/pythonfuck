10941d056fe82e295efff85c96327902f7e341a3
Claus Stolz <ruscoolman@gmail.com>
2017-06-06 18:44:12 +1000

    test interpreter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import interpreter
 
-test_code = '[++-ads.,a2r>da<[da++-.]]>.'
+test_code = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
 parsed_code = interpreter.parse_code(test_code)
 print(parsed_code)
 
 blocks = interpreter.parse_blocks_in_code(parsed_code)
 for i, j in blocks.items():
     print(i," ",j)
+
+f = open('output.txt', 'w')
+interpreter.run_code(test_code)

31567e9cb059cefcc200644eed13c86df247333e
Claus Stolz <ruscoolman@gmail.com>
2017-06-06 18:19:18 +1000

    add function in interpreter module, that executes the brainfuck code

--- interpreter.py
+++ interpreter.py
@@ -12,3 +12,30 @@ def parse_blocks_in_code(code):
             blocks[i] = block_start_array[-1]
             blocks[block_start_array.pop()] = i
     return blocks
+
+def run_code(code):
+    code = parse_code(code)
+    blocks = parse_blocks_in_code(code)
+    x = 0
+    i = 0
+    char_buffer = {0: 0}
+    while i < len(code):
+        char_value = code[i]
+        if (char_value == '>'):
+            x += 1
+            char_buffer.setdefault(x, 0)
+        elif (char_value == '<'):
+            x -= 1
+        elif (char_value == '+'):
+            char_buffer[x] += 1
+        elif (char_value == '-'):
+            char_buffer[x] -= 1
+        elif (char_buffer[x] == '.'):
+            print(chr(char_buffer[x]))
+        elif (char_value == '['):
+            if (not char_buffer[x]):
+                i = blocks[i]
+        elif (char_value == ']'):
+            if (char_buffer[x]):
+                i = blocks[i]
+        i += 1

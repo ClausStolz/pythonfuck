bebe08740c4b8d4997c571e88d7aff9eccb1a1f6
Claus Stolz <ruscoolman@gmail.com>
2017-06-06 17:55:39 +1000

    add function in interpreter module, that return a dictionary with begin and end of blocks

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,14 @@
 
 def parse_code(code):
     return ''.join(c for c in code if c in '><+-.,[]')
+
+def parse_blocks_in_code(code):
+    blocks = {}
+    block_start_array = []
+    for i in range(len(code)):
+        if code[i] = '[':
+            block_start_array.append(i)
+        elif code[i] = ']':
+            blocks[i] = block_start_array[-1]
+            blocks[block_start_array.pop()] = i
+    return blocks

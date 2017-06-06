77501af09f8ac3ea83475b4330925c0895806e20
Claus Stolz <ruscoolman@gmail.com>
2017-06-06 18:05:17 +1000

    test parse_blocks_in_code function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import interpreter
 
-test_code = '++-ads.,a2r>da<[da++-.]>.'
+test_code = '[++-ads.,a2r>da<[da++-.]]>.'
 parsed_code = interpreter.parse_code(test_code)
 print(parsed_code)
+
+blocks = interpreter.parse_blocks_in_code(parsed_code)
+for i, j in blocks.items():
+    print(i," ",j)

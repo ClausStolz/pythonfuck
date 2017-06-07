2883951b50ceacfa4e61f4275d6dd8db2b70e272
Claus Stolz <ruscoolman@gmail.com>
2017-06-07 10:20:27 +1000

    change main file

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import interpreter
 
 test_code = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
-parsed_code = interpreter.parse_code(test_code)
-print(parsed_code)
 
-blocks = interpreter.parse_blocks_in_code(parsed_code)
-for i, j in blocks.items():
-    print(i," ",j)
-
-f = open('output.txt', 'w')
-interpreter.run_code(test_code)
+bf = interpreter.BrainFuck(test_code)
+bf.run_code()
